c620c84a33f31635c0911dba07960284784c1d74
Kerli Aava <52652273+kerliaava@users.noreply.github.com>
2020-07-07 20:01:37 +0300

    scroll

--- js/background.js
+++ js/background.js
@@ -6,7 +6,7 @@ $(window).scroll(function () {
         $panel = $('.panel');
 
     // Change 33% earlier than scroll position so colour is there when you arrive.
-    var scroll = $window.scrollTop() + ($window.height() / 3);
+    var scroll = $window.scrollTop() + ($window.height() / 2);
 
     $panel.each(function () {
         var $this = $(this);
